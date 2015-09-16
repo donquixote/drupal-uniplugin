1def8032d1f1413050968cfdbae1a0252c303a38
Andreas Hennings <andreas@dqxtech.net>
2015-09-16 05:16:37 +0200

    Docblock PluginDefinitionBufferInterface

--- src/DefinitionsById/PluginDefinitionBufferInterface.php
+++ src/DefinitionsById/PluginDefinitionBufferInterface.php
@@ -4,6 +4,9 @@ namespace Drupal\uniplugin\DefinitionsById;
 
 use Drupal\uniplugin\IdToDefinition\IdToDefinitionInterface;
 
+/**
+ * Combination of two interfaces.
+ */
 interface PluginDefinitionBufferInterface extends DefinitionsByIdInterface, IdToDefinitionInterface {
 
 }
